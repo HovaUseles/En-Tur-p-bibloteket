b55c21ebd472eaa800985d3893ac32267f864f44
HovaUseles <bocaj.nielsen15@gmail.com>
2021-09-02 12:42:20 +0200

    Add project files.

---
+++ En Tur på bibloteket/Book.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace En_Tur_på_bibloteket
+{
+    class Book
+    {
+        private string name;
+        private string genre;
+        private string author;
+        private int pages;
+        private float price;
+        private bool fiction;
+
+        public Book()
+        {
+            // Empty constructor
+        }
+        public void GenerateBook()
+        {
+            Random rand = new Random();
+
+            // Setting up for generating random author and book name
+            string[] firstNames = new string[20] { "John", "Tom", "Michael", "Harrison", "Russell", "Isaac", "Jordan", "Alexander", "Ewan", "Gabriel", "Mia", "Jasmine", "Chelsea", "Madison", "Esme", "Olive", "Sierra", "Kali", "Cara", "Sofie" };
+            string[] lastNames = new string[20] { "Chambers", "Parker", "Burton", "Gordon", "Simpson", "Walsh", "Bennett", "Abbott", "Cameron", "Brady", "Rogers", "Spencer", "May", "Wilkinson", "Thompson", "Malone", "Short", "Beach", "Moon", "Doyle" };
+            string[] genreList = new string[7] { "Crime", "Fantasy", "Sci-Fi", "Mystery", "Horror", "Autobiography", "Learning Book" };
+            string[] bookNamePrefix = new string[20] { "Savior", "Goddess", "Swindlers", "Trees", "Pirates", "Humans", "Completion", "Luck", "Angel", "Defender", "Spiders", "Knights", "Docters", "Giants", "Anger", "Shield", "Traces", "Parrot", "Phantom", "Rebels" };
+            string[] bookNameMiddle = new string[5] { "of", "of the", "with", "without", "and" };
+            string[] bookNameSuffix = new string[20] { "Reality", "Tomorrow", "Time", "Water", "Earth", "Ocean", "Mountain", "Prison", "Forest", "End", "Money", "Vigor", "Wings", "Sins", "Pride", "A Home", "Fear", "Time", "Desire", "Sins" };
+            string bookName = "";
+            int prefixTemp = 0;
+            int middleTemp = 0;
+            int suffixTemp = 0;
+            prefixTemp = rand.Next(0, bookNamePrefix.Length);
+            middleTemp = rand.Next(0, bookNameMiddle.Length);
+
+            switch (middleTemp)
+            {
+                case 0:
+                    suffixTemp = rand.Next(0, 4);
+                    break;
+                case 1:
+                    suffixTemp = rand.Next(5, 9);
+                    break;
+                case 2:
+                    suffixTemp = rand.Next(10, 14);
+                    break;
+                case 3:
+                    suffixTemp = rand.Next(15, 19);
+                    break;
+            }
+            bookName = String.Concat(bookNamePrefix[prefixTemp], " ", bookNameMiddle[middleTemp]);
+            if (suffixTemp == 4)
+            {
+                bookName = String.Concat(bookName, " ", bookNamePrefix[rand.Next(0, bookNamePrefix.Length)]);
+            }
+            else
+            {
+                bookName = String.Concat(bookName, " ", bookNameSuffix[suffixTemp]);
+            }
+            this.Name = bookName;
+
+            // Setting author name
+            this.Author = String.Concat(firstNames[rand.Next(0, firstNames.Length)], " ", lastNames[rand.Next(0, lastNames.Length)]);
+            // Book genre
+            this.Genre = genreList[rand.Next(0, genreList.Length)];
+            this.Pages = rand.Next(80, 750);
+            this.Price = 9.99f + (5 * rand.Next(1, 8));
+            if (rand.Next(1, 100) < 50)
+            {
+                this.Fiction = true;
+            }
+            else
+            {
+                this.Fiction = false;
+            }
+
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Genre
+        {
+            get
+            {
+                return genre;
+            }
+            set
+            {
+                this.genre = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                this.author = value;
+            }
+        }
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                this.pages = value;
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+        public bool Fiction
+        {
+            get
+            {
+                return fiction;
+            }
+            set
+            {
+                this.fiction = value;
+            }
+        }
+    }
+}
---
+++ En Tur på bibloteket/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace En_Tur_på_bibloteket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List and stack we are using for the rental system
+            List<Book> booksForRental = new List<Book>();
+            Stack<Book> bookStack = new Stack<Book>();
+            Queue<Book> rentedBooks = new Queue<Book>();
+            // Used for while loops to stay in menu
+            bool looper = true;
+
+
+            // Adding books to the rental stack
+            void AddBookToStack()
+            {
+                while (looper)
+                {
+
+                    // First displaying available books
+                    Console.WriteLine("Currently available books\n");
+                    int counter = 1;
+                    foreach (Book hardBacks in booksForRental)
+                    {
+                        Console.WriteLine($"{counter}.");
+                        Console.WriteLine($"Name: {hardBacks.Name}");
+                        Console.WriteLine($"Author: {hardBacks.Author}");
+                        Console.WriteLine($"Genre: {hardBacks.Genre}");
+                        Console.WriteLine($"Pages: {hardBacks.Pages}");
+                        Console.WriteLine($"Price: {hardBacks.Price}$");
+                        if (hardBacks.Fiction)
+                        {
+                            Console.WriteLine("Fiction: Yes");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fiction: No");
+                        }
+                        Console.WriteLine();
+                        counter++;
+                    }
+                    //User chooses a book
+                    Console.WriteLine("Write the number on the book you want to rent. (0) to return to main menu");
+                    int input = int.Parse(Console.ReadLine());
+                    if (input == 0)
+                    {
+                        break;
+                    }
+
+                    // Adds the book from the available books to the rent stack and removes it from the original list
+                    bookStack.Push(booksForRental[input - 1]);
+                    booksForRental.RemoveAt(input - 1);
+                    Console.WriteLine("\nBook added");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+
+            }
+            void RentBook()
+            {
+                // loops this to mimic sub menu
+                while (looper)
+                {
+                    //  Show the next book in the stack and ask if the user wants to rent it and removes it if not
+                    Console.Clear();
+                    Console.WriteLine("Rent this book?\n");
+                    Book topBook = bookStack.Peek();
+                    Console.WriteLine($"Name: {topBook.Name}");
+                    Console.WriteLine($"Author: {topBook.Author}");
+                    Console.WriteLine($"Genre: {topBook.Genre}");
+                    Console.WriteLine($"Pages: {topBook.Pages}");
+                    Console.WriteLine($"Price: {topBook.Price}$");
+                    Console.WriteLine();
+                    Console.WriteLine("0. Exit\n1. Yes\n2. No");
+                    int input = int.Parse(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 0:
+                            return;
+                        case 1:
+                            rentedBooks.Enqueue(bookStack.Pop());
+                            Console.WriteLine("\nBook rented.");
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            booksForRental.Add(bookStack.Pop());
+                            Console.WriteLine("\nBook removed.");
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+            }
+            void ShowBooks()
+            {
+                if (bookStack.Count() == 0)
+                {
+                    Console.WriteLine("Book Stack empty");
+                }
+                foreach (Book hardBacks in bookStack)
+                {
+                    Console.WriteLine($"Name: {hardBacks.Name}");
+                    Console.WriteLine($"Author: {hardBacks.Author}");
+                    Console.WriteLine($"Genre: {hardBacks.Genre}");
+                    Console.WriteLine($"Pages: {hardBacks.Pages}");
+                    Console.WriteLine($"Price: {hardBacks.Price}$");
+                    if (hardBacks.Fiction)
+                    {
+                        Console.WriteLine("Fiction: Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fiction: No");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+
+            }
+            void RentedBooks()
+            {
+
+            }
+            // Generating random books
+            for (int i = 0; i < 10; i++)
+            {
+                Book book = new Book();
+                book.GenerateBook();
+                // Pauses on each interation to give program time to generate the book. Experiences a problem with the book being added before it was generated.
+                System.Threading.Thread.Sleep(50);
+                booksForRental.Add(book);
+            }
+
+            // Menu System
+            while (looper)
+            {
+                Console.Clear();
+                Console.WriteLine("==================================================");
+                Console.WriteLine("            H1 Queue Operations Menu");
+                Console.WriteLine("==================================================");
+                Console.WriteLine();
+                Console.WriteLine("1. Add book to rent stack");
+                Console.WriteLine("2. Rent books in stack");
+                Console.WriteLine("3. Show currunt book stack");
+                Console.WriteLine("4. Show current rented books");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine();
+                Console.WriteLine("Enter your choice");
+                Console.WriteLine();
+                int menuChoice = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (menuChoice)
+                {
+                    case 1:
+                        AddBookToStack();
+                        break;
+                    case 2:
+                        RentBook();
+                        break;
+                    case 3:
+                        ShowBooks();
+                        break;
+                    case 4:
+                        RentedBooks();
+                        break;
+                    case 5:
+                        looper = false;
+                        break;
+                    default:
+                        Console.WriteLine("Input not recognise. Please try igen");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+        }
+    }
+}
